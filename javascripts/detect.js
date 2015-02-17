75cce67752c2509153289de65e5a4f27bdd97cdd
Deomid Ryabkov <rojer@rojer.pro>
2015-02-18 00:51:38 +0300

    restore detect.js

--- javascripts/detect.js
+++ javascripts/detect.js
@@ -1,1 +1,46 @@
-var BrowserDetect={init:function(){this.browser=this.searchString(this.dataBrowser)||"An unknown browser",this.version=this.searchVersion(navigator.userAgent)||this.searchVersion(navigator.appVersion)||"an unknown version",this.OS=this.searchString(this.dataOS)||"an unknown OS"},searchString:function(a){for(var b=0;b<a.length;b++){var c=a[b].string,d=a[b].prop;if(this.versionSearchString=a[b].versionSearch||a[b].identity,c){if(-1!=c.indexOf(a[b].subString))return a[b].identity}else if(d)return a[b].identity}},searchVersion:function(a){var b=a.indexOf(this.versionSearchString);if(-1!=b)return parseFloat(a.substring(b+this.versionSearchString.length+1))},dataBrowser:[{string:navigator.userAgent,subString:"Chrome",identity:"Chrome"},{string:navigator.userAgent,subString:"OmniWeb",versionSearch:"OmniWeb/",identity:"OmniWeb"},{string:navigator.vendor,subString:"Apple",identity:"Safari",versionSearch:"Version"},{prop:window.opera,identity:"Opera"},{string:navigator.vendor,subString:"iCab",identity:"iCab"},{string:navigator.vendor,subString:"KDE",identity:"Konqueror"},{string:navigator.userAgent,subString:"Firefox",identity:"Firefox"},{string:navigator.vendor,subString:"Camino",identity:"Camino"},{string:navigator.userAgent,subString:"Netscape",identity:"Netscape"},{string:navigator.userAgent,subString:"MSIE",identity:"Explorer",versionSearch:"MSIE"},{string:navigator.userAgent,subString:"Gecko",identity:"Mozilla",versionSearch:"rv"},{string:navigator.userAgent,subString:"Mozilla",identity:"Netscape",versionSearch:"Mozilla"}],dataOS:[{string:navigator.platform,subString:"Win",identity:"Windows"},{string:navigator.platform,subString:"Mac",identity:"Mac"},{string:navigator.userAgent,subString:"iPhone",identity:"iPhone/iPod"},{string:navigator.platform,subString:"Linux",identity:"Linux"}]};BrowserDetect.init();
+var BrowserDetect = {
+  init: function(){
+    this.browser=this.searchString(this.dataBrowser)||"An unknown browser";
+    this.version=this.searchVersion(navigator.userAgent)||this.searchVersion(navigator.appVersion)||"an unknown version";
+    this.OS=this.searchString(this.dataOS)||"an unknown OS";
+  },
+  
+  searchString: function(a){
+    for (var b=0;b<a.length;b++){
+      var c=a[b].string, d=a[b].prop;
+      if(this.versionSearchString=a[b].versionSearch||a[b].identity,c){
+        if(-1!=c.indexOf(a[b].subString))return a[b].identity;
+      } else if(d)return a[b].identity;
+    }
+  },
+  
+  searchVersion: function(a){
+    var b=a.indexOf(this.versionSearchString);
+    if(-1!=b)return parseFloat(a.substring(b+this.versionSearchString.length+1))
+  },
+  
+  dataBrowser: [
+    {string:navigator.userAgent,subString:"Chrome",identity:"Chrome"},
+    {string:navigator.userAgent,subString:"OmniWeb",versionSearch:"OmniWeb/",identity:"OmniWeb"},
+    {string:navigator.vendor,subString:"Apple",identity:"Safari",versionSearch:"Version"},
+    {prop:window.opera,identity:"Opera"},
+    {string:navigator.vendor,subString:"iCab",identity:"iCab"},
+    {string:navigator.vendor,subString:"KDE",identity:"Konqueror"},
+    {string:navigator.userAgent,subString:"Firefox",identity:"Firefox"},
+    {string:navigator.vendor,subString:"Camino",identity:"Camino"},
+    {string:navigator.userAgent,subString:"Netscape",identity:"Netscape"},
+    {string:navigator.userAgent,subString:"MSIE",identity:"Explorer",versionSearch:"MSIE"},
+    {string:navigator.userAgent,subString:"Gecko",identity:"Mozilla",versionSearch:"rv"},
+    {string:navigator.userAgent,subString:"Mozilla",identity:"Netscape",versionSearch:"Mozilla"}
+  ],
+    
+  dataOS: [
+    {string:navigator.platform,subString:"Win",identity:"Windows"},
+    {string:navigator.platform,subString:"Mac",identity:"Mac"},
+    {string:navigator.userAgent,subString:"iPhone",identity:"iPhone/iPod"},
+    {string:navigator.platform,subString:"Linux",identity:"Linux"}
+  ]
+
+};
+
+BrowserDetect.init();
